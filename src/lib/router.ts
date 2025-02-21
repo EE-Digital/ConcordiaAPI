fc052d405bc7365acb4566a08fb8fe537986cf70
StarlitEvelyn <evelyn.rei@protonmail.com>
2025-02-21 21:25:09 +0100

    Added tea

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -24,7 +24,6 @@ export default async function runHTTPServer() {
 	const authDelete = authenticatedPathRegistrator(fastify, "DELETE");
 
 	fastify.addHook("onRequest", OpenCheck);
-
 	// Register routes
 
 	// Unauthenticated paths
--- src/middlewares/open.ts
+++ src/middlewares/open.ts
@@ -3,6 +3,7 @@ import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from "fastify";
 import config from "../../config.json";
 
 export default function OpenCheck(req: FastifyRequest, res: FastifyReply, done: HookHandlerDoneFunction) {
+	if (config.open == "tea") return res.status(418).send("I'm a teapot.");
 	if (config.open) return done();
 	res.status(400).send("This API is closed.");
 }
