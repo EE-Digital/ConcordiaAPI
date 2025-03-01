f2ba7a9f4d16ca1564c90ea55e6f8cfd4a386c63
StarlitEvelyn <evelyn.rei@protonmail.com>
2025-03-01 16:10:22 +0100

    Partial setup script

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,25 @@ import chalk from "chalk";
 import createAdmin from "./modules/debug.js";
 import runHTTPServer from "./modules/router.js";
 import updateHandler from "./modules/update.js";
+import setup from "./modules/setup.js";
 
 if (process.env.DEV) {
 	console.log(chalk.blue("Running in DEV mode"));
 	createAdmin();
 }
 
-// Start the server
-runHTTPServer();
+// Setup the server if it's not already setup
+if (!process.env.DATABASE_URL) {
+	setup().then(() => process.exit(0));
+} else startServer();
 
-// Start update handler
-updateHandler();
-setInterval(() => {
+function startServer() {
+	// Start API server
+	runHTTPServer();
+
+	// Start update handler
 	updateHandler();
-}, 1000 * 60 * 60 * 25); // 25 hours
+	setInterval(() => {
+		updateHandler();
+	}, 1000 * 60 * 60 * 25); // 25 hours
+}
